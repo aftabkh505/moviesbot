9155c0393505b21ec53491124246ebc8c0dd4b73
anandvfcvijay <49161738+anandvfcvijay@users.noreply.github.com>
2019-06-08 20:56:39 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -48,9 +48,14 @@ def echo(bot, update):
             parse_mode=pyrogram.ParseMode.HTML
         )
         return
-        # logger.info(response_json)
-        keyboard = [[InlineKeyboardButton(text="ðŸ¤– Support Group", url="https://t.me/KeralasBots")]]
-        update.effective_message.reply_text(reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
+      bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.ABUSIVE_USERS,
+            reply_to_message_id=update.message_id,
+            disable_web_page_preview=True,
+            parse_mode=pyrogram.ParseMode.HTML
+      )
+       
         
 
 
