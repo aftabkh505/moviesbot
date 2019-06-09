423331cf2ef172ecff38d2d364fc673298fe69b7
anandvfcvijay <49161738+anandvfcvijay@users.noreply.github.com>
2019-06-09 22:03:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,4 +8,4 @@ class Translation(object):
 © For support join this [group](https://t.me/joinchat/JoNrMhTGJlh3DDHLkmTITg)"""   
     HELP_USER = """coming soon """
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    THUPPAKKI = "[@imdb](https://imgur.com/a/skQP941)\n\n`TITLE : THUPPAKI`\n`YEAR : 2019`\n`LANGUAGE : TAMIL`"
+    THUPPAKKI = "[@imdb](https://imgur.com/a/skQP941)\n\n`TITLE : THUPPAKI`\n`YEAR : 2012`\n`LANGUAGE : TAMIL`"
