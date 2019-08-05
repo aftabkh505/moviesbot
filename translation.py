819072269dfb0e4344459e19ae9bc227158ae3d8
Anand P S <49161738+anandvfcvijay@users.noreply.github.com>
2019-08-05 20:19:43 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,6 @@
 class Translation(object):
-    START_TEXT = """Thank You for using me.
-
-/help to know how to use me
-
-
-
-© For support join this [group](https://t.me/joinchat/JoNrMhTGJlh3DDHLkmTITg)"""   
-    HELP_USER = """coming soon """
+    START_TEXT = """`Hi! I am a bot which creates a bridge with Anand `"""   
+    HELP_USER = """`Hi! I am a bot which creates a bridge with Anand `"""
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     THUPPAKKI = "[@imdb](https://imgur.com/a/skQP941)\n\n`TITLE : THUPPAKI`\n`YEAR : 2012`\n`LANGUAGE : TAMIL`"
+    CHAT_TEXT = "Hi will reply to the message soon.."
